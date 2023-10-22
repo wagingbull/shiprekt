9387e03004574a5e34e37c019ffd590646ae508b
WagingBull <wagingbull@proton.me>
2023-10-22 00:30:02 -0400

    basic gold clicker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,59 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { AccountForm } from '../components/AccountForm/AccountForm';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-interface AccountFormValues {
-  address: string;
-}
+
 const HomePage: NextPage = () => {
-  const router = useRouter();
+  const [goldCount, setGoldCount] = useState(0);
+  const [minerCount, setMinerCount] = useState(0);
+  const [winMessage, setWinMessage] = useState('');
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const [recordTime, setRecordTime] = useState(0);
 
-  const handleSubmit = (values: AccountFormValues) => {
-    router.push(`/inventory/${values.address}`);
+  useEffect(() => {
+    let savedGoldCount, savedMinerCount, savedRecordTime;
+    if (typeof window !== 'undefined') {
+      savedGoldCount = localStorage.getItem('goldCount');
+      savedMinerCount = localStorage.getItem('minerCount');
+      savedRecordTime = localStorage.getItem('recordTime');
+    }
+    setGoldCount(savedGoldCount ? parseInt(savedGoldCount) : 0);
+    setMinerCount(savedMinerCount ? parseInt(savedMinerCount) : 0);
+    setRecordTime(savedRecordTime ? parseInt(savedRecordTime) : 0);
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('goldCount', goldCount.toString());
+      localStorage.setItem('minerCount', minerCount.toString());
+    }
+    if (minerCount >= 10) {
+      setWinMessage('You win!');
+      setEndTime(Date.now());
+      const elapsedTime = endTime - startTime;
+      if (elapsedTime < recordTime || recordTime === 0) {
+        setRecordTime(elapsedTime);
+        localStorage.setItem('recordTime', elapsedTime.toString());
+      }
+      setGoldCount(0);
+      setMinerCount(0);
+    }
+  }, [goldCount, minerCount]);
+
+  const increaseGold = () => {
+    if (startTime === 0) {
+      setStartTime(Date.now());
+    }
+    setGoldCount(goldCount + 1 + minerCount);
+  };
+
+  const hireMiner = () => {
+    if (goldCount >= 10) {
+      setGoldCount(goldCount - 10);
+      setMinerCount(minerCount + 1);
+    }
   };
 
   return (
@@ -27,11 +68,18 @@ const HomePage: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Link href="/inventory/0x000000000000000000000000000000000000dead">
-          <img src="/favicon.jpg" alt="Click for example Cryptopunk" />
-        </Link>
+        <h1>Gold: {goldCount}</h1>
+        <h1>Miners: {minerCount}</h1>
+        {winMessage && <h2>{winMessage}</h2>}
+        {recordTime > 0 && <h2>Record Time: {recordTime}ms</h2>}
 
-     
+        <button onClick={increaseGold}>
+          Click to increase gold count by {1 + minerCount}
+        </button>
+
+        <button onClick={hireMiner} disabled={goldCount < 10}>
+          Hire a miner for 10 gold
+        </button>
       </main>
 
       <footer className={styles.footer}>
@@ -47,3 +95,5 @@ const HomePage: NextPage = () => {
 };
 
 export default HomePage;
+
+
